d615390ec14a9e27eb28ba26f80ecc6f77177470
Demian Schkolnik <demianschkolnik@gmail.com>
2017-08-17 12:01:55 -0300

    Plot near neighbours 1

---
+++ Voidfinding/plotPoints.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+
+def plot(name,centerPointsPython,outlierPointsPython,borderPointsPython):
+
+   centerPointsNP = np.array(centerPointsPython)
+   outlierPointsNP = np.array(outlierPointsPython)
+   borderPointsNP = np.array(borderPointsPython)
+
+   plt.figure()
+   plt.title(name)
+   plt.plot(borderPointsNP[:,0], borderPointsNP[:,1], 'ko', markersize=1.2, color='blue', label="Border Points")
+   plt.plot(centerPointsNP[:,0], centerPointsNP[:,1], 'ko', markersize=1.2 , color='cyan', label="Center Points")
+   plt.plot(outlierPointsNP[:,0], outlierPointsNP[:,1], 'ko', markersize=1.2, color='green', label="Outlier Points")
+
+   plt.legend(loc=4, fontsize='small')
+
+   plt.xlim(-1050, 1050)
+   plt.ylim(-1050, 1050)
+
+   plt.show()
+
+def plotWithEpsilonNeighbour(name, centerPointsPython, outlierPointsPython, borderPointsPython, edge_points):
+   centerPointsNP = np.array(centerPointsPython)
+   outlierPointsNP = np.array(outlierPointsPython)
+   borderPointsNP = np.array(borderPointsPython)
+
+   plt.figure()
+   plt.title(name)
+   plt.plot(borderPointsNP[:, 0], borderPointsNP[:, 1], 'ko', markersize=1.2, color='blue', label="Border Points")
+   plt.plot(centerPointsNP[:, 0], centerPointsNP[:, 1], 'ko', markersize=1.2, color='cyan', label="Center Points")
+   plt.plot(outlierPointsNP[:, 0], outlierPointsNP[:, 1], 'ko', markersize=1.2, color='green',
+            label="Outlier Points")
+
+   lines = LineCollection(edge_points, color='black', linewidths=0.2)
+   plt.gca().add_collection(lines)
+
+   print("asasd")
+   # x1 = [-100, 120]
+   # y1 = [100, 400]
+   # plt.plot(x1, y1, marker='None', lw=1)
+
+   plt.legend(loc=4, fontsize='small')
+
+   plt.xlim(-1050, 1050)
+   plt.ylim(-1050, 1050)
+
+   plt.show()
--- Voidfinding/testMain.py
+++ Voidfinding/testMain.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.spatial import Delaunay
-import plotDelaunay
+import plotPoints as pp
+
+#from scipy.spatial import Delaunay
+#import plotDelaunay
 
 # Se necesita fijar dos parámetros, una distancia epsilon y un número de
 # vecinos k. Dados estos parámetros, las estrellas se clasifican en tres
@@ -22,23 +24,17 @@ import plotDelaunay
 # las aristas entre estrellas tipo borde, para definir los vacío.
 
 def distance(x1,y1,x2,y2):
-    "distance between (x1,y1) and (x2,y2)"
-    deltaXSquare = (x1-x2)**2
-    deltaYSquare = (y1-y2)**2
-    return (deltaXSquare+deltaYSquare) ** (0.5)
+   "distance between (x1,y1) and (x2,y2)"
+   deltaXSquare = (x1-x2)**2
+   deltaYSquare = (y1-y2)**2
+   return (deltaXSquare+deltaYSquare) ** (0.5)
 
 
-epsilon    = 50                #epsilon is the distance to check for neighbours
-k          = 4                  #k is the number on the epsilon-neighborhood criterion
-file       = 'Data/20irr2d_1024.dat'   #File to be read
-plotPoints = False              #Plot the points?
-plotDelau  = True               #Plot Delaunay?
-
-
-if plotPoints:
-    plt.plotfile('data1.dat', delimiter=' ', cols=(0, 1),
-             names=('col1', 'col2'),linestyle="", marker='.')
-    plt.show()
+epsilon    = 100 #epsilon is the distance to check for neighbours
+k          = 8 #k is the number on the epsilon-neighborhood criterion
+file       = 'Data/20irr2d_1024.dat' #File to be read
+plot = True #Plot?
+plotNearestNeighbour = True #Plot lines to epsilon-neighbours?
 
 
 
@@ -47,65 +43,81 @@ f = open(file, 'r')
 i = 0
 first = 0
 for r in f:
-    if first == 0:
-        dimensions = int(r)
-        first += 1
-    elif first == 1:
-        l = int(r)
-        first += 1
-        raw = np.zeros((l,2))
-    else:
-        row = r.split()
-        raw[i,0] = float(row[0])
-        raw[i,1] = float(row[1])
-        i += 1
+   if first == 0:
+       dimensions = int(r)
+       first += 1
+   elif first == 1:
+       l = int(r)
+       first += 1
+       raw = np.zeros((l,2))
+   else:
+       row = r.split()
+       raw[i,0] = float(row[0])
+       raw[i,1] = float(row[1])
+       i += 1
 
 
 M = np.zeros((l, l))
 
 #Filling distance Matrix M
 for i in range(0,l):
-    for j in range(0,i):
-        dist = distance(raw[i,0],raw[i,1],raw[j,0],raw[j,0])
-        M[i,j] = dist
-        M[j,i] = dist
+   for j in range(0,i):
+       dist = distance(raw[i,0],raw[i,1],raw[j,0],raw[j,1])
+       M[i,j] = dist
+       M[j,i] = dist
 
 #Ids
 center = []
 outlier = []
+candidates = []
 border = []
 
 centerPointsPython = []
 outlierPointsPython = []
+candidatesPointsPython = []
 borderPointsPython = []
+
+neighbourEdge_points = []
+neighbour_edges = set()
+
+def add_edge(i, j):
+    """Add a line between the i-th and j-th points, if not in the list already"""
+    if (i, j) in neighbour_edges or (j, i) in neighbour_edges:
+        return
+    neighbour_edges.add((i, j))
+    neighbourEdge_points.append(raw[ [i, j] ])
 
 #check for center objects.
 for i in range(0,l):
-    nrNeigh = 0
-    for j in range(0,l):
-        if (M[i,j] <= epsilon) and i != j:
-            nrNeigh += 1
-    if nrNeigh >= k:
-        centerPointsPython.append(raw[i].tolist())
-        center.append(i)
-    else:
-        outlier.append(i)
-        outlierPointsPython.append(raw[i].tolist())
+   nrNeigh = 0
+   for j in range(0,l):
+       if (M[i,j] <= epsilon) and i != j:
+           nrNeigh += 1
+           if plotNearestNeighbour:
+               add_edge(i,j)
+   if nrNeigh >= k:
+       centerPointsPython.append(raw[i].tolist())
+       center.append(i)
+   else:
+       candidates.append(i)
+       candidatesPointsPython.append(raw[i].tolist())
 
 #Move candidates from outlier to border
-for cand in outlier:
-    wasBorder = False
-    for c in center:
-        if M[cand,c] <= epsilon:
-            border.append(cand)
-            borderPointsPython.append(raw[cand].tolist())
-            wasBorder = True
-            break
-    if wasBorder:
-        outlier.remove(cand)
-        outlierPointsPython.remove(raw[cand].tolist())
+for cand in candidates:
+   wasBorder = False
+   for c in center:
+       if M[cand,c] <= epsilon:
+           border.append(cand)
+           borderPointsPython.append(raw[cand].tolist())
+           wasBorder = True
+           break
+   if not wasBorder:
+       outlier.append(cand)
+       outlierPointsPython.append(raw[cand].tolist())
 
-
-if plotDelau :
-    plotDelaunay.plot(centerPointsPython,outlierPointsPython,borderPointsPython,raw,epsilon)
-
+if plot:
+    plotName = 'Data:' + file + ' | epsilon:' + str(epsilon) + ' | k:' + str(k)
+    if plotNearestNeighbour:
+        pp.plotWithEpsilonNeighbour(plotName, centerPointsPython, outlierPointsPython, borderPointsPython,neighbourEdge_points)
+    else:
+        pp.plot(plotName, centerPointsPython, outlierPointsPython, borderPointsPython)
